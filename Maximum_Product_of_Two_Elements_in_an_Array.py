e5c3ad77a4b46c4559d48a816fae40b3def494da
kalyankol <103946030+kalyankol@users.noreply.github.com>
2022-06-28 18:40:10 +0530

    l=list(map(int,input().split())) max=0 for i in range(len(l)):     for j in range(i+1,len(l)):         c=(l[i]-1)*(l[j]-1)         #print(c)         if c>max:             max=c print(max)

---
+++ Maximum_Product_of_Two_Elements_in_an_Array.py
@@ -0,0 +1,9 @@
+l=list(map(int,input().split()))
+max=0
+for i in range(len(l)):
+    for j in range(i+1,len(l)):
+        c=(l[i]-1)*(l[j]-1)
+        #print(c)
+        if c>max:
+            max=c
+print(max)
