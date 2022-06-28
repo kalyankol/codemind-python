0068cb61434b1d30b64aec320fd89aa61dff3132
kalyankol <103946030+kalyankol@users.noreply.github.com>
2022-06-28 19:15:36 +0530

    def fib(l):     a,b=0,1     #print(a,b,end=' ')     c1=0     for i in range(2,l):         c=a+b         if(l==c):             c1=1         b,a=c,b     if c1==1:         print(True)     else:         print(False) n=int(input()) fib(n)

---
+++ Fibonacci_or_not.py
@@ -0,0 +1,15 @@
+def fib(l):
+    a,b=0,1
+    #print(a,b,end=' ')
+    c1=0
+    for i in range(2,l):
+        c=a+b
+        if(l==c):
+            c1=1
+        b,a=c,b
+    if c1==1:
+        print(True)
+    else:
+        print(False)
+n=int(input())
+fib(n)
